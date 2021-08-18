bc1dce2874e5c81f0289d0a1f11f4802f7f14d9d
scarlett1130 <businessx.free2000@gmail.com>
2021-08-18 16:16:14 -0400

    testing status update feature

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import {
   StreamApp,
   NotificationDropdown,
   FlatFeed,
-  Activity,
   LikeButton,
+  Activity,
+  CommentList,
+  CommentField,
+  StatusUpdateForm,
 } from "react-activity-feed";
 import "react-activity-feed/dist/index.css";
 
@@ -17,7 +20,9 @@ class App extends React.Component {
         token="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiYnJvYWQtYm9udXMtMCIsImV4cCI6MTYyOTMzMDA1MX0.f9ZtqBz0t6A8VMykxWj3q25PFySxLA3LVbnNmF-rdAA"
       >
         <NotificationDropdown notify />
+        <StatusUpdateForm />
         <FlatFeed
+          options={{ reactions: { recent: true } }}
           notify
           Activity={(props) => (
             <Activity
@@ -25,6 +30,11 @@ class App extends React.Component {
               Footer={() => (
                 <div style={{ padding: "8px 16px" }}>
                   <LikeButton {...props} />
+                  <CommentField
+                    activity={props.activity}
+                    onAddReaction={props.onAddReaction}
+                  />
+                  <CommentList activityId={props.activity.id} />
                 </div>
               )}
             />
